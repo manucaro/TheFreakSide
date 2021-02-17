cacf2c74a6a363edc9b67615eab05e0b5eb63f70
manucaro <79217115+manucaro@users.noreply.github.com>
2021-02-17 18:25:57 +0100

    Add files via upload

---
+++ TheFreakSide/TheFreakSide/TheFreakSideContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using TheFreakSide.Models;
+
+namespace TheFreakSide
+{
+    public class TheFreakSideContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source=DESKTOP-CGCIHMP\\SQLEXPRESS;Initial Catalog=Polis;Integrated Security=True");
+        }
+
+        public TheFreakSideContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        public TheFreakSideContext()
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<ContactEnquirie> ContactEnquiries { get; set; }
+    }
+}
