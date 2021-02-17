0a485864a0296c9c01f2300bbe277a2e30dd3402
manucaro <79217115+manucaro@users.noreply.github.com>
2021-02-17 18:22:22 +0100

    Add files via upload

---
+++ TheFreakSide/TheFreakSide/TheFreakSide/Controllers/ContactEnquiriesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheFreakSide.Models;
+
+namespace TheFreakSide.Controllers
+{
+    public class ContactEnquiriesController : Controller
+    {
+        private TheFreakSideContext context = new TheFreakSideContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SuccessContactEnquirie()
+        {
+            return View("SuccessContactEnquirie");
+        }
+
+        [HttpPost]
+        public ActionResult Index(ContactEnquirie contactEnquirie)
+        {
+            InsertContactEnquirie(contactEnquirie);
+            return SuccessContactEnquirie();
+        }
+
+        // POST: ContactController/Create
+        public void InsertContactEnquirie(ContactEnquirie contactEnquirie)
+        {
+            context.Add(contactEnquirie);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ TheFreakSide/TheFreakSide/TheFreakSide/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheFreakSide.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public string ImageURL { get; set; }
+        public int StockQuantity { get; set; }
+        public int SoldQuantity { get; set;  }
+    }
+}
